15b431b650e4f7e81b5ecb0219f22677214c0901
u0125812 <ferdinand.gerdener@gmail.com>
2023-11-07 16:34:12 +0100

    Benutzer iz da

--- src/main/java/Benutzer.java
+++ src/main/java/Benutzer.java
@@ -4,4 +4,52 @@ public class Benutzer {
     protected String nachname;
     protected String password;
     protected int kontonummer;
+
+    public Benutzer(int idBenutzer, String vorname, String nachname, String password, int kontonummer) {
+        this.idBenutzer = idBenutzer;
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.password = password;
+        this.kontonummer = kontonummer;
+    }
+
+    public int getIdBenutzer() {
+        return idBenutzer;
+    }
+
+    public void setIdBenutzer(int idBenutzer) {
+        this.idBenutzer = idBenutzer;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getKontonummer() {
+        return kontonummer;
+    }
+
+    public void setKontonummer(int kontonummer) {
+        this.kontonummer = kontonummer;
+    }
 }
