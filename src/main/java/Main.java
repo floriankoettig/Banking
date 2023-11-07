355caf35802f33b2a04dac846988ae73388e1887
Köttig <Florian.Koettig@keb.de>
2023-11-07 17:08:17 +0100

    create table 'Konto', 'Benutzer'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,19 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         var conn = DbConnection.getConnection();
         var statement = conn.createStatement();
+
+        statement.execute("""
+            
+            CREATE TABLE IF NOT EXISTS "Benutzer" (
+            "idBenutzer" UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+            "vorname" varchar(255) not null,
+            "nachname" varchar(255) not null,
+            "password" varchar (255) not null,
+            FOREIGN KEY "kontonummer" REFERENCES "Konto"
+            );
+            CREATE TABLE IF NOT EXISTS "Konto" (
+            "kontonummer" serial PRIMARY KEY
+            "benutzerID" int REFERENCES "Benutzer")
+            """);
     }
 }
