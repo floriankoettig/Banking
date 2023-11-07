c2c93c83df321524598a0464e9358ac77f8a606e
u0125812 <ferdinand.gerdener@gmail.com>
2023-11-07 16:32:35 +0100

    Benutzer iz da

---
+++ src/main/java/Benutzer.java
@@ -0,0 +1,7 @@
+public class Benutzer {
+    protected int idBenutzer;
+    protected String vorname;
+    protected String nachname;
+    protected String password;
+    protected int kontonummer;
+}
