61c26230281834bafcbafb8ffe149b1807ce7077
floriankoettig <floriankoettig@gmx.de>
2023-11-09 09:52:21 +0100

    benutzerverwaltung

--- src/main/java/Benutzerverwaltung.java
+++ src/main/java/Benutzerverwaltung.java
@@ -1,9 +1,13 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Anmeldung {
+public class Benutzerverwaltung {
     private static Map<String, String> benutzerdaten = new HashMap<>();
+    Connection conn = DbConnection.getConnection();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -22,6 +26,7 @@ public class Anmeldung {
                 System.out.print("Passwort eingeben: ");
                 String passwort = scanner.nextLine();
                 benutzerdaten.put(benutzername, passwort);
+                Benutzerverwaltung.registriereBenutzer();
                 System.out.println("Benutzer erfolgreich registriert.");
             } else if (option == 2) {
                 System.out.print("Benutzername eingeben: ");
@@ -37,6 +42,25 @@ public class Anmeldung {
             } else if (option == 3) {
                 break;
             }
+        }
+    }
+    private static void registriereBenutzer(String vorname, String nachname, String passwort, int kontonummer) {
+        try {
+            String insertQuery = "INSERT INTO \"Benutzer\" (\"vorname\", \"nachname\", \"password\", \"kontonummer\") VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(insertQuery);
+
+            preparedStatement.setString(1, vorname);
+            preparedStatement.setString(2, nachname);
+            preparedStatement.setString(3, passwort);
+            preparedStatement.setInt(4, kontonummer);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+            System.out.println("Benutzer erfolgreich registriert.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("Fehler beim Registrieren des Benutzers.");
         }
     }
 }
